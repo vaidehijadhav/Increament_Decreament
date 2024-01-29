b68df0efee23f2c4cbf851b8e32962130fc5039c
Vaidehi Jadhav <jadhav.v10122@gmail.com>
2024-01-29 19:24:02 +0530

    Add increment and decrement function

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-const countValue = document.querySelector('#counter');
+const countValue = document.querySelector('#counter'); //querySelector return value in form of string
 
 const increment = ()=>{
-
+    // get the value
+    let value = parseInt(countValue.innerText); // parseInt convert string value to interger.
+    // update the value
+    value++;
+    // set the value
+    countValue.innerText = value;
 };
 
 const decrement = () =>{
-
+    // get the value
+    let value = parseInt(countValue.innerText); // parseInt convert string value to interger.
+    // update the value
+    value--;
+    // set the value
+    countValue.innerText = value;
 };
